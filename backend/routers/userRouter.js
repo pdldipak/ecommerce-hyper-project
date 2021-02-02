07a442f332c6edf585aa8134135eda672c978ba7
Dipak Poudel <pdl.dipak@gmail.com>
2021-02-02 23:03:18 +0100

    signin for user implemented in backend

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 import data from '../data.js';
 import User from '../models/User.js';
+import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
 
@@ -9,13 +11,34 @@ userRouter.get('/hyperproject', async (req, res) => {
     const createdUsers = await User.insertMany(data.users);
     res.send({ createdUsers });
   } catch (error) {
-    console.error(error);
     res.status(500).send({ message: error.message });
   } finally {
     console.log('We do cleanup here');
-   // await User.remove({});
+    // await User.remove({});
   }
   return 'Nothing found';
+});
+
+userRouter.post('/signin', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    } else {
+      res.status(401).send({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
 });
 
 export default userRouter;
