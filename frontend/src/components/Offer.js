c7b05deeddd317b224b9e9778909ba22110cdb2c
Dipak Poudel <pdl.dipak@gmail.com>
2021-05-04 06:52:16 +0200

    media query fixed

--- frontend/src/components/Offer.js
+++ frontend/src/components/Offer.js
@@ -8,32 +8,33 @@ function Offer({ product }) {
     <Fade top>
       <div className='offer-row' key={product._id} id='offer'>
         <div className='offer-col-2'>
-          <h1>
-            Special discount to new customers
-            <br />
-            For Limited time
-          </h1>
-          <p>
-            Anim culpa et proident ut mollit minim in ad nisi Lorem
-            minim ullamco ea sunt. Consequat voluptate anim labore eu
-            anim fugiat sit ullamco esse exercitation exercitation
-            cupidatat exercitation. Ex qui laboris ad officia occaecat
-            excepteur cillum ad. <br /> Cillum anim labore occaecat
-            dolor. Dolore reprehenderit elit dolore id in adipisicing.
-            Fugiat Lorem veniam in consectetur occaecat occaecat in
-            voluptate dolore ut adipisicing quis. Commodo nisi qui
-            duis deserunt fugiat exercitation id deserunt labore
-            mollit ex aute
-          </p>
           <div>
-            <NavLink to={`/product/${product._id}`}>
-              <button className='btn-special'>
-                <span>
-                  Buy Now {' '}
-                  <i className='bi bi-arrow-right' />
-                </span>
-              </button>
-            </NavLink>
+            <h1>
+              Special discount to new customers
+              <br />
+              For Limited time
+            </h1>
+            <p>
+              Anim culpa et proident ut mollit minim in ad nisi Lorem
+              minim ullamco ea sunt. Consequat voluptate anim labore
+              eu anim fugiat sit ullamco esse exercitation
+              exercitation cupidatat exercitation. Ex qui laboris ad
+              officia occaecat excepteur cillum ad. <br /> Cillum anim
+              labore occaecat dolor. Dolore reprehenderit elit dolore
+              id in adipisicing. Fugiat Lorem veniam in consectetur
+              occaecat occaecat in voluptate dolore ut adipisicing
+              quis. Commodo nisi qui duis deserunt fugiat exercitation
+              id deserunt labore mollit ex aute
+            </p>
+            <div>
+              <NavLink to={`/product/${product._id}`}>
+                <button className='btn-special'>
+                  <span>
+                    Buy Now <i className='bi bi-arrow-right' />
+                  </span>
+                </button>
+              </NavLink>
+            </div>
           </div>
         </div>
         <div className='offer-col-2'>
